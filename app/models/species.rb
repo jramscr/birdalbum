cb29c78a3b53a62be8479794af7bded4d68388fa
jramoscr <jramos@pernix-solutions.com>
2015-06-02 19:29:57 -0600

    Fix associations typos

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,7 +1,7 @@
 class Species < ActiveRecord::Base
   belongs_to :gender
   has_many :color_by_species
-  has_many :colors
+  has_many :colors, through: :color_by_species
   has_many :species_by_zones
-  has_many :zones, through: :species_by_zones, through: :color_by_species
+  has_many :zones, through: :species_by_zones
 end
