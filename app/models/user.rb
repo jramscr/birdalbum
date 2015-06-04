11df085c2d0535d663ea96b0511cd80577f7bb6e
jramoscr <jramos@pernix-solutions.com>
2015-06-04 02:50:49 -0600

    Remvoe email field like neccessary space

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
-  validates_presence_of :username, :password, :password_confirmation
+  validates_presence_of :username, :password, :password_confirmation, on: :create
 
   belongs_to :person
   belongs_to :user_type
