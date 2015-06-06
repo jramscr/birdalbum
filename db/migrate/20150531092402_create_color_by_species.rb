8f7862905628e2758dcac333c7e67bcfc40b4f0a
jramoscr <jramos@pernix-solutions.com>
2015-06-06 09:30:26 -0600

    Removed color_by_species association

--- db/migrate/20150531092402_create_color_by_species.rb
+++
@@ -1,8 +0,0 @@
-class CreateColorBySpecies < ActiveRecord::Migration
-  def change
-    create_table :color_by_species do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20150531093618_add_ref_color_and_ref_species_to_color_by_species.rb
+++
@@ -1,6 +0,0 @@
-class AddRefColorAndRefSpeciesToColorBySpecies < ActiveRecord::Migration
-  def change
-    add_reference :color_by_species, :color, index: true
-    add_reference :color_by_species, :species, index: true
-  end
-end
