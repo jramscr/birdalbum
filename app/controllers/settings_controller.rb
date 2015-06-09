311838f9a62168f7955a98e2e549337608b33654
jramoscr <jramos@pernix-solutions.com>
2015-06-09 09:19:58 -0600

    Filter incertae_sedis birds

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,9 @@
 class SettingsController < ApplicationController
   load_and_authorize_resource
 
-  def dashboard
+  def dashboard; end
+
+  def insertaes
+    @birds = Bird.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'settings/dashboard'
+  get 'settings/insertaes'
 
   devise_for :users
   resources :contact_types
